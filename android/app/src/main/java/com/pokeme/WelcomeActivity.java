7441efd33b44e710b73bc8283f94e4d3f7fdffeb
Bahattin Cinic <bahattin@adphorus.com>
2018-02-18 20:18:15 +0300

    Added dummy activities

--- android/app/src/main/java/com/pokeme/WelcomeActivity.java
+++ android/app/src/main/java/com/pokeme/WelcomeActivity.java
@@ -1,7 +1,9 @@
 package com.pokeme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void openLoginActivity(View view) {
+        Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void openSignupActivity(View view) {
+        Intent intent = new Intent(WelcomeActivity.this, SignupActivity.class);
+        startActivity(intent);
     }
 }
