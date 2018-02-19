adba86d542dba845e0b132863b27285cf7f54a68
Bahattin Cinic <bahattin@adphorus.com>
2018-02-19 21:14:06 +0300

    Added validations

--- android/app/src/main/java/com/pokeme/LoginActivity.java
+++ android/app/src/main/java/com/pokeme/LoginActivity.java
@@ -1,13 +1,40 @@
 package com.pokeme;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class LoginActivity extends AppCompatActivity {
+    TextView txtUsername;
+    TextView txtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txtUsername = (TextView) findViewById(R.id.txtLoginUsername);
+        txtPassword = (TextView) findViewById(R.id.txtLoginPassword);
+    }
+
+    public void onSubmit(View view) {
+        String username = txtUsername.getText().toString();
+        String password = txtPassword.getText().toString();
+        Context context = getApplicationContext();
+
+        if (TextUtils.isEmpty(username)) {
+            Toast.makeText(context, getString(R.string.username_required_message), Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(context, getString(R.string.password_required_message), Toast.LENGTH_LONG).show();
+            return;
+        }
     }
 }
--- android/app/src/main/java/com/pokeme/SignupActivity.java
+++ android/app/src/main/java/com/pokeme/SignupActivity.java
@@ -1,13 +1,40 @@
 package com.pokeme;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class SignupActivity extends AppCompatActivity {
+    TextView txtUsername;
+    TextView txtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        txtUsername = (TextView) findViewById(R.id.txtSignupUsername);
+        txtPassword = (TextView) findViewById(R.id.txtSignupPassword);
+    }
+
+    public void onSubmit(View view) {
+        String username = txtUsername.getText().toString();
+        String password = txtPassword.getText().toString();
+        Context context = getApplicationContext();
+
+        if (TextUtils.isEmpty(username)) {
+            Toast.makeText(context, getString(R.string.username_required_message), Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(context, getString(R.string.password_required_message), Toast.LENGTH_LONG).show();
+            return;
+        }
     }
 }
