c8222451dcca795628e3b0b6eb233d66a5223869
Bahattin Cinic <bahattin@adphorus.com>
2018-02-23 18:26:52 +0300

    Added empty pages

---
+++ android/app/src/main/java/com/pokeme/NoteCreateActivity.java
@@ -0,0 +1,13 @@
+package com.pokeme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoteCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_create);
+    }
+}
