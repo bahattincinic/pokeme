2fcd22ee7f7275e69e8d44fcfa77ce8812227ab4
Bahattin Cinic <bahattin@adphorus.com>
2018-02-26 23:49:52 +0300

    Add menu

--- android/app/src/main/java/com/pokeme/DashboardActivity.java
+++ android/app/src/main/java/com/pokeme/DashboardActivity.java
@@ -1,14 +1,26 @@
 package com.pokeme;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.google.gson.Gson;
 import com.pokeme.models.User;
+import com.pokeme.store.Session;
 
 public class DashboardActivity extends AppCompatActivity {
     User user;
+
+    private DrawerLayout drawer;
+    private NavigationView navigation;
+    private Session session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +29,44 @@ public class DashboardActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         user = new Gson().fromJson(intent.getStringExtra("user"), User.class);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.menu);
+
+        session = Session.getInstance(this);
+
+        navigation = (NavigationView) findViewById(R.id.nav_view);
+        navigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.nav_logout:
+                        session.clearSession();
+                        Intent intent = new Intent(DashboardActivity.this, WelcomeActivity.class);
+                        finish();
+                        startActivity(intent);
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- android/app/src/main/java/com/pokeme/WelcomeActivity.java
+++ android/app/src/main/java/com/pokeme/WelcomeActivity.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.android.volley.VolleyError;
@@ -20,6 +21,7 @@ import org.json.JSONObject;
 public class WelcomeActivity extends AppCompatActivity {
     ProgressDialog dialog;
     NetworkManager queue;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
