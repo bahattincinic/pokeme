39d4d65480e645441c1b9a9a86bd63b58a41a41a
Bahattin Cinic <bahattin@adphorus.com>
2018-03-01 22:18:14 +0300

    Added splash activity

---
+++ android/app/src/main/java/com/pokeme/SplashActivity.java
@@ -0,0 +1,65 @@
+package com.pokeme;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.pokeme.service.NetworkManager;
+import com.pokeme.service.UserService;
+import com.pokeme.service.VolleyCallback;
+import com.pokeme.store.Session;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class SplashActivity extends AppCompatActivity {
+    ProgressDialog dialog;
+    NetworkManager queue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        queue = NetworkManager.getInstance(this);
+        String token = Session.getInstance(this).getToken();
+
+        if (token == null || token.isEmpty()) {
+            return;
+        }
+
+        dialog = new ProgressDialog(SplashActivity.this);
+        dialog.setMessage(getString(R.string.loading));
+        dialog.setCancelable(false);
+        dialog.show();
+
+        try {
+            JsonObjectRequest request = UserService.getProfile(token, new VolleyCallback() {
+                @Override
+                public void onSuccess(JSONObject instance) {
+                    Intent intent = new Intent(SplashActivity.this, DashboardActivity.class);
+                    intent.putExtra("user", instance.toString());
+
+                    dialog.hide();
+                    finish();
+                    startActivity(intent);
+                }
+
+                @Override
+                public void onError(VolleyError error) {
+                    Intent intent = new Intent(SplashActivity.this, WelcomeActivity.class);
+                    dialog.hide();
+                    finish();
+                    startActivity(intent);
+                }
+            });
+            queue.add(request);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- android/app/src/main/java/com/pokeme/WelcomeActivity.java
+++ android/app/src/main/java/com/pokeme/WelcomeActivity.java
@@ -1,66 +1,17 @@
 package com.pokeme;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.pokeme.service.NetworkManager;
-import com.pokeme.store.Session;
-import com.pokeme.service.UserService;
-import com.pokeme.service.VolleyCallback;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 
 public class WelcomeActivity extends AppCompatActivity {
-    ProgressDialog dialog;
-    NetworkManager queue;
-    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-
-        queue = NetworkManager.getInstance(this);
-        String token = Session.getInstance(this).getToken();
-
-        if (token == null || token.isEmpty()) {
-            return;
-        }
-
-        dialog = new ProgressDialog(WelcomeActivity.this);
-        dialog.setMessage(getString(R.string.loading));
-        dialog.setCancelable(false);
-        dialog.show();
-
-        try {
-            JsonObjectRequest request = UserService.getProfile(token, new VolleyCallback() {
-                @Override
-                public void onSuccess(JSONObject instance) {
-                    Intent intent = new Intent(WelcomeActivity.this, DashboardActivity.class);
-                    intent.putExtra("user", instance.toString());
-
-                    dialog.hide();
-                    finish();
-                    startActivity(intent);
-                }
-
-                @Override
-                public void onError(VolleyError error) {
-                    dialog.hide();
-                }
-            });
-            queue.add(request);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
     }
 
     public void openLoginActivity(View view) {
